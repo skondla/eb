f9b15d0cc915bc79667b07c8c26e3eb29f93ee65
skondla <CloudArchitectUSA@gmail.com>
2015-11-17 20:32:04 -0500

    Delete findReplace.sh

--- findReplace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find eb/ -type f -exec sed -i -e 's/skondla@cisco.com/sudheer.kondla@gmail.com/g' {} \;
-#find ./ -type f -exec sed -i -e 's/sudheer.kondla@gmail.com/skondla@cisco.com/g' {} \;
-#find . -name "*.*" | xargs grep "sudheer.kondla@gmail.com"
-find eb/ -name "*.*" | xargs grep "skondla@cisco.com"
